115de74277bd2efacb6e12425e7d6b3321173dbf
houimliraed <houimliraed@outlook.fr>
2025-10-17 01:38:02 +0100

    first commit

---
+++ backend/app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="Choose your Own Adventure API",
+    description="API to generate cool stories",
+    version="1.0",
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.get("/")
+def home():
+    return {'hello':'world'}
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app",host="0.0.0.0",port=5000,reload=True)
