f67523e4f7020b4587ec390bb899ed73a72503aa
houimliraed <houimliraed@outlook.fr>
2025-11-12 22:25:26 +0100

    added db config

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+# importing the settings
+from core.config import settings
+
+
+
 
 app = FastAPI(
     title="Choose your Own Adventure API",
@@ -11,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins= settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
---
+++ backend/app/models/story.py
@@ -0,0 +1,7 @@
+
+from sqlalchemy import Column,String,Integer,DateTime,Boolean,ForeignKey,JSON 
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+class Story():
+    
