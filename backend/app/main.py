739b37e76857a86a90537a5ebe958fa8cf5a8900
houimliraed <houimliraed@outlook.fr>
2025-12-12 21:53:44 +0100

    added frontend configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,34 +1,26 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-# importing the settings
-from core.config import settings
 
 
 
 
 app = FastAPI(
-    title="Choose your Own Adventure API",
-    description="API to generate cool stories",
-    version="1.0",
+    title="Choose your own adventure Game API",
+    description="JUST An API To Generate Some AI Stuff",
+    version="1.O",
     docs_url="/docs",
     redoc_url="/redoc"
 )
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= settings.ALLOWED_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
 
-@app.get("/")
-def home():
-    return {'hello':'world'}
-
-
-
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app",host="0.0.0.0",port=5000,reload=True)
+    uvicorn.run("main:app",host="0.0.0.0",port=8001,reload=True)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <p>
+      Hello there !
+    </p>
+    </>
+  )
+}
+
+export default App
