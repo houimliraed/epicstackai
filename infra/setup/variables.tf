c56741560c662cb0e45d2f824c27deeb672fd3fa
Raed Houimli <houimli.raed@tek-up.de>
2025-12-19 19:04:57 +0100

    fix

--- infra/setup/variables.tf
+++ infra/setup/variables.tf
@@ -10,7 +10,3 @@ variable "Contact" {
   description = "Contact info for tagging resouces"
   default     = "houimliraed@engineergrid.com"
 }
-variable "frontend_bucket_arn" {
-  description = "ARN of the frontend S3 bucket"
-  type        = string
-}
