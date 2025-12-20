f8fae32eafb29f9455c849a3975d8432d463e6ba
Raed Houimli <houimli.raed@tek-up.de>
2025-12-20 09:04:01 +0100

    fixed uneeded data

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -1,12 +1,21 @@
+#########################
+# RDS Subnet Group
+#########################
 resource "aws_db_subnet_group" "main" {
   name       = "${var.prefix}-main"
   subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
-  tags = { Name = "${var.prefix}-db-subnet-group" }
+
+  tags = {
+    Name = "${var.prefix}-db-subnet-group"
+  }
 }
 
+#########################
+# RDS Security Group
+#########################
 resource "aws_security_group" "rds" {
-  description = "Allow access to RDS instances"
   name        = "${var.prefix}-rds-inbound-access"
+  description = "Allow access to RDS instances"
   vpc_id      = aws_vpc.main.id
 
   ingress {
@@ -16,11 +25,21 @@ resource "aws_security_group" "rds" {
     cidr_blocks = [aws_vpc.main.cidr_block]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "${var.prefix}-rds-security-group"
   }
 }
 
+#########################
+# RDS Instance
+#########################
 resource "aws_db_instance" "main" {
   identifier                 = "${local.prefix}-db"
   db_name                    = "recipe"
@@ -28,18 +47,17 @@ resource "aws_db_instance" "main" {
   storage_type               = "gp2"
   engine                     = "postgres"
   engine_version             = "15.10"
-  auto_minor_version_upgrade = true
   instance_class             = "db.t4g.micro"
   username                   = var.db_username
   password                   = var.db_password
   skip_final_snapshot        = true
-  db_subnet_group_name       = aws_db_subnet_group.main.name  # <- remove [0]
   multi_az                   = false
   backup_retention_period    = 0
+
+  db_subnet_group_name       = aws_db_subnet_group.main.name
   vpc_security_group_ids     = [aws_security_group.rds.id]
 
   tags = {
     Name = "${local.prefix}-main"
   }
 }
-
