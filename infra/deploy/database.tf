19ed7b7e4413bba1fcd032a7fad80955f3c066bc
Raed Houimli <houimli.raed@tek-up.de>
2025-12-20 09:01:24 +0100

    updated database.tf

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -33,7 +33,7 @@ resource "aws_db_instance" "main" {
   username                   = var.db_username
   password                   = var.db_password
   skip_final_snapshot        = true
-  db_subnet_group_name       = aws_db_subnet_group.main[0].name
+  db_subnet_group_name       = aws_db_subnet_group.main.name  # <- remove [0]
   multi_az                   = false
   backup_retention_period    = 0
   vpc_security_group_ids     = [aws_security_group.rds.id]
@@ -42,3 +42,4 @@ resource "aws_db_instance" "main" {
     Name = "${local.prefix}-main"
   }
 }
+
